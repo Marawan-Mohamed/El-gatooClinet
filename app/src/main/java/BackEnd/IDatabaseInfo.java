9709e1b64db7c6d6a42946a7de61f7bd60aa27fb
Marwan Mohammed <85809132+Marawan-Mohamed@users.noreply.github.com>
2023-06-13 22:30:03 +0300

    Add files via upload

---
+++ app/src/main/java/BackEnd/IDatabaseInfo.java
@@ -0,0 +1,12 @@
+package BackEnd;
+// Interface that holds database location, user and password
+
+interface IDatabaseInfo {
+
+    String url = "jdbc:mysql://n1nlmysql13plsk.secureserver.net:3306/chat";
+    String user = "chat2112";
+    String password = "Chat14@12!";
+//    String url = "jdbc:mysql://192.168.1.9:3306/mostafa";
+//    String user = "mostafa2";
+//    String password = "mostafa2";
+}
